9d25c584f30908e169a10f7f099dffa8c99675be
Shubhangi <Shubhangi@DESKTOP-0CKGN4V>
2025-02-19 13:25:39 +0100

    Added get business summary by prompt function

--- src/gen-ai-controllers/business-summary.ts
+++ src/gen-ai-controllers/business-summary.ts
@@ -154,6 +154,16 @@ bsRouter.use("/", (request: Request, response: Response, next: NextFunction) => 
             });
     }
 
-});
+}).get("/get-business-summary", async (request: Request, response: Response) => {
+    try {
+        let fid = <string>request.query.fid;
+        let promptId = Number(request.query.promptId);
+        var collection = appService.mongooseConnection.collection('businessSummaries');
+        let businessSummary = await collection.findOne({ fid: new ObjectId(fid), promptId :  promptId});
+        response.status(200).json(businessSummary).end();
+    } catch (error) {
+        response.status(500).send().end();
+    }
+});;
 
 module.exports = bsRouter;
