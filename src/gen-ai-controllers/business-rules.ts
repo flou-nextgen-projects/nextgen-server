0b58b561b7787c15168574ddfd55889da13c29ac
Shubhangi <Shubhangi@DESKTOP-0CKGN4V>
2025-02-26 06:21:54 +0100

    changes-for-regen-business-rules

--- src/gen-ai-controllers/business-rules.ts
+++ src/gen-ai-controllers/business-rules.ts
@@ -1,6 +1,7 @@
 import Express, { Request, Response, Router, NextFunction } from "express";
 import { appService } from "../services/app-service";
 import { ObjectId } from "mongodb";
+import Mongoose from "mongoose";
 
 const brRouter: Router = Express.Router();
 brRouter.use("/", (request: Request, response: Response, next: NextFunction) => {
@@ -42,5 +43,13 @@ brRouter.use("/", (request: Request, response: Response, next: NextFunction) => 
         return response.status(404).json({ message: 'business Summary Update API Error' }).end();
     }
     response.status(200).json(({ message: `${data.type} Update Successfully`, data })).end();
-})
+}).get("/get-business-rule/:fid/:promptId", (request: Request, response: Response) => {
+    const { fid, promptId } = request.params;
+    appService.mongooseConnection.collection("businessRules").findOne({ fid: Mongoose.Types.ObjectId.createFromHexString(fid), promptId: parseInt(promptId) })
+        .then((res) => {
+            response.status(200).json(res).end();
+        }).catch((err) => {
+            response.status(500).json(err).end();
+        })
+});
 module.exports = brRouter;
