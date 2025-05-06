fecf705829f141027c7e01980e628c44ac9fc6f4
Snehal Pawar <pawar.snehal@hotmail.com>
2025-05-06 15:57:51 +0530

    stash

--- mongo-server.bat
+++ mongo-server.bat
@@ -1,5 +1,5 @@
 @CD/
 @C:
-@CD "C:\Program Files\MongoDB\Server\8.0\bin"
-@REM mongod.exe --dbpath E:\mongodb-databases --directoryperdb --port 27000 --bind_ip 127.0.0.1 --auth
-@mongod.exe --port 27000 --bind_ip 127.0.0.1 --auth --dbpath E:\mongodb-databases --directoryperdb --tlsMode requireTLS --tlsCertificateKeyFile "E:\nextgen-projects\nextgen-server\dist\certificates\mongodb-server.pem" --tlsCAFile "E:\nextgen-projects\nextgen-server\dist\certificates\rootCA.pem"
+@CD "C:\Program Files\MongoDB\Server\7.0\bin"
+@ mongod.exe --dbpath D:\next-gen-projects\next-gen-db-26032025 --directoryperdb --port 27000 --bind_ip 127.0.0.1 
+@ REM mongod.exe --port 27000 --bind_ip 127.0.0.1 --auth --dbpath E:\mongodb-databases --directoryperdb --tlsMode requireTLS --tlsCertificateKeyFile "E:\nextgen-projects\nextgen-server\dist\certificates\mongodb-server.pem" --tlsCAFile "E:\nextgen-projects\nextgen-server\dist\certificates\rootCA.pem"
